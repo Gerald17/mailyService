56c912326354a883183edd766f9b45be0f126afd
Gerald <gerardo.estrada.dg@gmail.com>
2018-11-21 16:00:50 -0600

    trying again

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ hi: 'there buddy'});
 });
 
 // heroku
-
-
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
