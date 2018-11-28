ddb0a172a72856053ca9e5ecfef45e70dd36f486
gerardo_estrada <gerardo.estrada.dg@gmail.com>
2018-11-28 16:31:26 -0600

    change vars_env

--- config/prodKeys.js
+++ config/prodKeys.js
@@ -10,4 +10,4 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY
 }
 
-// production {"web":{"client_id":"858317661100-ofl2srmjpgg88cva4780r8091pm8bj1b.apps.googleusercontent.com","project_id":"maily-dev-223304","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"eR3RgmIYOUEilLT_KZUWcENh"}}
+// production {"web":{"client_id":"496604403355-nr04fh9gqb1kgel9nd8c1selirsoc8bl.apps.googleusercontent.com","project_id":"maily-prod-223921","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"bgMY3GmRIB1zaHUI8um6k4OG","redirect_uris":["https://fast-woodland-26950.herokuapp.com/auth/google/callback"],"javascript_origins":["https://fast-woodland-26950.herokuapp.com"]}}
